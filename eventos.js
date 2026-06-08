21b6de8dfcde503c91cf75aa8c81eef6d2eeaf3a
Steven Morales Alvarado <steve@LAPTOP-P959UABJ>
2026-06-07 21:09:41 -0600

    Se termino el css de la pagina de registro y se agregaron eventos iniciales.

---
+++ eventos.js
@@ -0,0 +1,140 @@
+function InicioSesion_Registro() {
+  // Nota el punto "." antes de Registrar, funciona como en CSS
+  const elemento = document.querySelector(".Registrar");
+  const elementoPadre = document.querySelector(".InicioSesion");
+
+  if (elemento.style.display === "flex") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "flex"; // Cambié este a "flex" para que se muestre
+  }
+
+  elementoPadre.style.display = "none";
+}
+
+function Registro_Perfil() {
+  // Nota el punto "." antes de Registrar, funciona como en CSS
+  const elemento = document.querySelector(".Perfil");
+  const elementoPadre = document.querySelector(".Registrar");
+
+  if (elemento.style.display === "block") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "block"; // Cambié este a "flex" para que se muestre
+  }
+
+  elementoPadre.style.display = "none";
+}
+
+function Perfil_Configurar() {
+  // Nota el punto "." antes de Registrar, funciona como en CSS
+  const elemento = document.querySelector(".ConfigurarPerfil");
+  const elementoPadre = document.querySelector(".Perfil");
+
+  if (elemento.style.display === "Flex") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "Flex"; // Cambié este a "flex" para que se muestre
+  }
+
+  elementoPadre.style.display = "none";
+}
+
+function Configurar_Perfil() {
+  // Nota el punto "." antes de Registrar, funciona como en CSS
+  const elemento = document.querySelector(".Perfil");
+  const elementoPadre = document.querySelector(".ConfigurarPerfil");
+
+  if (elemento.style.display === "block") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "block"; // Cambié este a "flex" para que se muestre
+  }
+
+  elementoPadre.style.display = "none";
+}
+
+function Configurar_InicioSesion() {
+  // Nota el punto "." antes de Registrar, funciona como en CSS
+  const elemento = document.querySelector(".InicioSesion");
+  const elementoPadre = document.querySelector(".ConfigurarPerfil");
+
+  if (elemento.style.display === "Flex") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "Flex"; // Cambié este a "flex" para que se muestre
+  }
+
+  elementoPadre.style.display = "none";
+}
+
+function Perfil_InicioSesion() {
+  // Nota el punto "." antes de Registrar, funciona como en CSS
+  const elemento = document.querySelector(".InicioSesion");
+  const elementoPadre = document.querySelector(".Perfil");
+
+  if (elemento.style.display === "Flex") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "Flex"; // Cambié este a "flex" para que se muestre
+  }
+
+  elementoPadre.style.display = "none";
+}
+
+
+function Hero1_Hero2() {
+  // Nota el punto "." antes de Registrar, funciona como en CSS
+  const elemento = document.querySelector(".heroSection2");
+  const elementoPadre = document.querySelector(".heroSection1");
+
+  if (elemento.style.display === "grid") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "grid"; // Cambié este a "flex" para que se muestre
+  }
+
+  elementoPadre.style.display = "none";
+}
+
+function Hero2_Hero3() {
+  // Nota el punto "." antes de Registrar, funciona como en CSS
+  const elemento = document.querySelector(".heroSection3");
+  const elementoPadre = document.querySelector(".heroSection2");
+
+  if (elemento.style.display === "grid") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "grid"; // Cambié este a "flex" para que se muestre
+  }
+
+  elementoPadre.style.display = "none";
+}
+
+function Hero3_Hero4() {
+  // Nota el punto "." antes de Registrar, funciona como en CSS
+  const elemento = document.querySelector(".heroSection4");
+  const elementoPadre = document.querySelector(".heroSection3");
+
+  if (elemento.style.display === "grid") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "grid"; // Cambié este a "flex" para que se muestre
+  }
+
+  elementoPadre.style.display = "none";
+}
+
+function Hero4_Hero1() {
+  // Nota el punto "." antes de Registrar, funciona como en CSS
+  const elemento = document.querySelector(".heroSection1");
+  const elementoPadre = document.querySelector(".heroSection4");
+
+  if (elemento.style.display === "flex") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "flex"; // Cambié este a "flex" para que se muestre
+  }
+
+  elementoPadre.style.display = "none";
+}
